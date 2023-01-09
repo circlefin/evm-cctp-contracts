14d274090fc1730ad7cd729596ad4aae49efb3cd
Harman Singh <harman.singh@circle.com>
2023-01-09 15:45:30 -0800

    [STABLE-2112]: Update script and add documentation

--- examples/README.md
+++ examples/README.md
@@ -1,3 +1,52 @@
 
 # Quickstart: Cross-chain USDC Transfer
-TODO
+
+This example uses [Ethers.js](https://docs.ethers.org/v5/getting-started/) and [Metamask](https://metamask.io/) to transfer USDC from address on ETH testnet to another address AVAX testnet. The script has 4 steps and can be run in 2 parts. Run steps 1 to 3 require Goerli testnet selected in Metamask with associated active address. Step 4 require changing the network to Avalance Fuji testnet with associated active address.
+
+Following guide provides step by step instruction on how to run the script:
+1. Start a local server by running `python3 -m http.server 8000` in the folder.
+2. Switch to Goerli test network and active address with some USDC and ETH in Metamask browser extension.
+3. Go to `http://localhost:8000` in browser and keep the console opened.
+4. When running this script first time, you will notice a badge appear on Metamask extension. It needs permission to be able to connect to the site (localhost).
+```js
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+const signer = provider.getSigner()
+```
+5. Next, call the `depositForBurn` function on `TokenMessengerContract` deployed in [Goerli testnet](https://goerli.etherscan.io/address/0xd0c3da58f55358142b8d3e06c1c30c5c6114efe8). Metamask will ask for this transaction to be approved.
+```js
+const burnTx = await ethTokenMessengerContract.depositForBurn(amount, AVAX_DESTINATION_DOMAIN, destinationAddressInBytes32, USDC_CONTRACT_ADDRESS);
+```
+*Note: When running this function on your own node, you can use ERC20 [approve function](https://docs.openzeppelin.com/contracts/2.x/api/token/erc20) as described in step 0 of the script to allow `TokenMessengerContract` to withdraw USDC from your account.*
+```js
+const approveTx = await usdcEthContract.approve(TOKEN_MESSENGER_CONTRACT_ADDRESS, 500000) // 0.5 USDC
+```
+
+6. We then need to extract `messageBytes` from the above transaction logs.
+```js
+const transactionReceipt = await provider.getTransactionReceipt(burnTx.hash);
+const eventTopic = ethers.utils.id('MessageSent(bytes)')
+const log = transactionReceipt.logs.find((l) => l.topics[0] === eventTopic)
+const messageBytes = ethers.utils.defaultAbiCoder.decode(['bytes'], log.data)[0]
+```
+7. Hash the retrived `messageBytes` using `keccak256` algorithm.
+```js
+const messageHash = ethers.utils.keccak256(messageBytes);
+```
+8. Call attestation service to acquire signature using the `messageHash` from previous step.
+```js
+const response = await fetch(`https://iris-api-sandbox.circle.com/attestations/${messageHash}`);
+const attestationResponse = await response.json()
+const signature = attestationResponse.attestation;
+```
+9. Switch to Avalanche Fuji Network and active address with some AVAX in Metamask.
+10. Populate the below fields in step 4 of the script using the `messageBytes` and `signature` gathered in previous steps.
+```js
+const receivingMessageBytes = '<ADD_MESSAGE_BYTES_HERE>'
+const signature = '<ADD_SIGNATURE_HERE>'
+```
+11. Call `receiveMessage` function on `TokenMessengerContract` deployed in [Avalanche Fuji Network](https://testnet.snowtrace.io/address/0xa9fb1b3009dcb79e2fe346c16a604b8fa8ae0a79)
+```js
+const receiveTx = await avaxMessageTransmitterContract.receiveMessage(receivingMessageBytes, signature);
+```
+12. Allow Metamask to complete the transaction. 
+13. Check for transaction with `ReceiveMessage` method on [block explorer](https://testnet.snowtrace.io/).
---
+++ examples/index.js
@@ -0,0 +1,72 @@
+import tokenMessengerAbi from './abis/TokenMessenger.json' assert { type: 'json' };
+import messageTransmitterAbi from './abis/MessageTransmitter.json' assert { type: 'json' };
+import messageAbi from './abis/Message.json' assert { type: 'json' };
+import usdcAbi from './abis/Usdc.json' assert { type: 'json' };
+
+async function main() {
+    // Connect to Metamask Wallet
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+
+    // Contract Addresses
+    const ETH_TOKEN_MESSENGER_CONTRACT_ADDRESS = "0xd0c3da58f55358142b8d3e06c1c30c5c6114efe8";
+    const USDC_ETH_CONTRACT_ADDRESS = "0x07865c6e87b9f70255377e024ace6630c1eaa37f";
+    const ETH_MESSAGE_CONTRACT_ADDRESS = "0x1a9695e9dbdb443f4b20e3e4ce87c8d963fda34f"
+    const AVAX_MESSAGE_TRANSMITTER_CONTRACT_ADDRESS = '0xa9fb1b3009dcb79e2fe346c16a604b8fa8ae0a79';
+    
+    // initialize contracts using address and ABI
+    const ethTokenMessengerContract = new ethers.Contract(ETH_TOKEN_MESSENGER_CONTRACT_ADDRESS, tokenMessengerAbi, signer);
+    const usdcEthContract = new ethers.Contract(USDC_ETH_CONTRACT_ADDRESS, usdcAbi, signer);
+    const ethMessageContract = new ethers.Contract(ETH_MESSAGE_CONTRACT_ADDRESS, messageAbi, signer);
+    const avaxMessageTransmitterContract = new ethers.Contract(AVAX_MESSAGE_TRANSMITTER_CONTRACT_ADDRESS, messageTransmitterAbi, signer);
+
+    // AVAX destination address
+    const mintRecipient = "<YOUR_AVAX_ADDRESS>";
+    const destinationAddressInBytes32 = await ethMessageContract.addressToBytes32(mintRecipient);
+    const AVAX_DESTINATION_DOMAIN = 1;
+    
+    // STEP 0: Approve messenger contract to withdraw from our active metamask address
+    // const approveTx = await usdcEthContract.approve(TOKEN_MESSENGER_CONTRACT_ADDRESS, 500000) // 0.5 USDC
+    // console.log(`ApproveTx: ${approveTx.hash}`)
+    
+    // Wait for transaction to complete
+    // const approveTxReceipt = await provider.waitForTransaction(approveTx.hash)
+    // console.log(`ApproveTxReceipt: ${approveTxReceipt}`)
+    
+    // STEP 1: Burn USDC
+    const amount = 100000 // 0.1 USDC
+    const burnTx = await ethTokenMessengerContract.depositForBurn(amount, AVAX_DESTINATION_DOMAIN, destinationAddressInBytes32, USDC_CONTRACT_ADDRESS);
+    console.log(`BurnTx: ${burnTx}`)
+
+    // Wait for transaction to complete
+    const burnTxReceipt = await provider.waitForTransaction(burnTx.hash)
+    console.log(`BurnTxReceipt: ${burnTxReceipt}`)
+
+    // STEP 2: Retrieve message bytes from logs
+    const transactionReceipt = await provider.getTransactionReceipt(burnTx.hash);
+    const eventTopic = ethers.utils.id('MessageSent(bytes)')
+    const log = transactionReceipt.logs.find((l) => l.topics[0] === eventTopic)
+    const messageBytes = ethers.utils.defaultAbiCoder.decode(['bytes'], log.data)[0]
+    const messageHash = ethers.utils.keccak256(messageBytes);
+
+    console.log(`MessageBytes: ${messageBytes}`)
+    console.log(`MessageHash: ${messageHash}`)
+
+    // STEP 3: Fetch attestation signature
+    const response = await fetch(`https://iris-api-sandbox.circle.com/attestations/${messageHash}`);
+    const attestationResponse = await response.json()
+    while(attestationResponse.status != 'complete') {
+        await new Promise(r => setTimeout(r, 2000));
+    }
+
+    const attestationSignature = attestationResponse.attestation;
+    console.log(`Signature: ${attestationSignature}`)
+
+    // STEP 4: Using the message bytes and signature recieve the funds on destination chain and address
+    const receivingMessageBytes = '<ADD_MESSAGE_BYTES_HERE>'
+    const signature = '<ADD_SIGNATURE_HERE>'
+    const receiveTx = await avaxMessageTransmitterContract.receiveMessage(receivingMessageBytes, signature);
+    console.log(`ReceiveTx: ${receiveTx}`)
+}
+
+main()
