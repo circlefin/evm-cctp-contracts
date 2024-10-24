b09984509f833ec4985d4d9fb5bb3ec08aec6a56
ams9198 <111915188+ams9198@users.noreply.github.com>
2024-10-24 16:50:38 -0400

    Add AddressUtilsExternal library with external functions (#40)

---
+++ src/messages/v2/AddressUtilsExternal.sol
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 Circle Internet Group, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+pragma solidity 0.7.6;
+
+/**
+ * @title AddressUtilsExternal Library
+ * @notice Helper functions for converting addresses to and from bytes
+ **/
+library AddressUtilsExternal {
+    /**
+     * @notice converts address to bytes32 (alignment preserving cast.)
+     * @param addr the address to convert to bytes32
+     */
+    function addressToBytes32(address addr) external pure returns (bytes32) {
+        return bytes32(uint256(uint160(addr)));
+    }
+
+    /**
+     * @notice converts bytes32 to address (alignment preserving cast.)
+     * @dev Warning: it is possible to have different input values _buf map to the same address.
+     * For use cases where this is not acceptable, validate that the first 12 bytes of _buf are zero-padding.
+     * @param _buf the bytes32 to convert to address
+     */
+    function bytes32ToAddress(bytes32 _buf) external pure returns (address) {
+        return address(uint160(uint256(_buf)));
+    }
+}
